445f994fd80b7f604661126d88618c72e867d049
Travis Puryear <travispuryear678@gmail.com>
2021-11-13 12:19:08 -0500

    All routes for API for Workout model.

--- controllers/api.js
+++ controllers/api.js
@@ -2,6 +2,24 @@ const express = require("express");
 const router = express.Router();
 const Workout = require("../models/workout");
 
+
+
+// GET for all workouts. 
+router.get("/workouts", (req,res) => {
+    
+    Workout.find().then((workoutData) => {
+        res.json(workoutData)
+    }).catch((err) => {
+        res.json(err)
+    })
+})
+
+
+
+
+
+
+// POST create a new workout. 
 router.post("/workouts", ({ body }, res) => {
   Workout.create(body)
     .then((workoutData) => {
@@ -11,8 +29,29 @@ router.post("/workouts", ({ body }, res) => {
       res.json(err);
     });
 });
-router.put("/workouts/:id", ({ body, params }, res) => {
-  Workout.update({});
+
+// Update workout by it's ID and push in new body to database object. 
+router.put("/workouts/:id", ({body,params}, res) => {
+    const filter = {_id : params.id};
+    const update = {$push: {exercises: body}};
+
+  Workout.findOneAndUpdate(filter,update).then((updatedWorkoutData) => {
+      res.json(updatedWorkoutData)
+
+  }).catch((err) => {
+      res.json(err)
+  })
+      
 });
+
+// GET for range of all workouts
+router.get("/workouts/range", (req,res) => {
+    Workout.find().then((workoutData) => {
+        res.json(workoutData)
+    }).catch((err) => {
+        res.json(err)
+    })
+})
+
 
 module.exports = router;
