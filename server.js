0b3c51dcac48f13839106172888955b062751175
Travis Puryear <travispuryear678@gmail.com>
2021-11-13 10:44:09 -0500

    Intial barebones express server setup.

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 const express = require('express')
+const app = express();
+const PORT = 3000 || process.env.port 
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`);
+})
