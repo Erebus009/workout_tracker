25d7221d04b601f8cdd42907f8bd07ac7bdd24ed
Travis Puryear <travispuryear678@gmail.com>
2021-11-13 14:58:34 -0500

    heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const { urlencoded } = require("body-parser");
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT|| 3000 ;
 const controllers = require("./controllers");
 const app = express();
 
